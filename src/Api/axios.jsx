4b812cddf3c7ef487bfba7af2608ac8fd031ad24
AliAkber0 <aliakber654@gmail.com>
2022-09-16 14:41:04 +0500

    todo app

---
+++ src/Api/axios.jsx
@@ -0,0 +1,12 @@
+import axios from "axios";
+import { BASE_URL } from "../Utils/const";
+
+const axioxInstance = axios.create({ baseURL: BASE_URL });
+
+export const getDataUsingAxios = async () => {
+  const response = await axioxInstance.request({
+    method: "GET",
+    url: "/todos",
+  });
+  return response.data;
+};
