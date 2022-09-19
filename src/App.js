19dc7a1139ab85bf89dc275e9e8cb3a3fc6d3e53
ShoaibAshiq <sho.imran9671@gmail.com>
2022-09-19 13:12:16 +0500

    converted task 1

--- src/App.js
+++ src/App.js
@@ -1,109 +1,99 @@
-import { Component } from "react";
+import { useState } from "react";
 import "./App.scss";
-const axios = require('axios');
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: "",
-      currentId: null,
-      todos: ["wroking on todo APP"],
-      withFetch: [],
-      withAxios: [],
-    };
-    this.addTodo = this?.addTodo?.bind(this);
-    this.deleteTodo = this?.deleteTodo?.bind(this);
-    this.updateTodo = this?.updateTodo?.bind(this);
-  }
+function App() {
+  const [todos, setTodos] = useState({
+    value: "",
+    currentId: null,
+    list: ["wroking on todo APP"],
+  });
 
-  addTodo() {
-    this.setState({ todos : [this.state.value, ...this.state.todos] })
-    this.setState({ value : ""})
-  }
-
-  updateTodo() {
-    if (this.state.currentId != null && this.state.value != "") {
-      this.setState({
-        todos: [
-          this.state.value,
-          ...this.state.todos.filter((todo, i) => i != this.state.currentId),
-        ],
-      });
-      this.setState({ value: "" });
+  function AddToDo() {
+    if (todos.currentId == null && todos.value != "") {
+      setTodos({ list: [todos.value, ...todos.list], value: "" });
     }
   }
 
-  deleteTodo(index) {
-    const todos = this.state.todos.filter((todo, i) => i != index);
-    this.setState({ todos });
+  function updateTodo() {
+    if (todos.currentId != null && todos.value != "") {
+      setTodos({
+        list: [
+          todos.value,
+          ...todos.list.filter((x, i) => i != todos?.currentId),
+        ],
+        value: "",
+      });
+    }
   }
 
-  render() {
-    return (
-      <div className="App">
-        <div className="container">
-          <h1>TODO APP</h1>
-          <div className="todo">
-            <input
-              type="text"
-              value={this.state.value}
-              placeholder="add todo"
-              onChange={(e) => this.setState({ value: e.target.value })}
-            />
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>TODO APP</h1>
+        <div className="todo">
+          <input
+            type="text"
+            value={todos?.value}
+            placeholder="add todo"
+            onChange={(e) => setTodos({ ...todos, value: e.target.value })}
+          />
+          <button
+            className="add-btn"
+            disabled={todos?.value?.trim().length <= 0}
+            onClick={() => AddToDo()}
+          >
+            Add
+          </button>
+          <button
+            className="upd-btn"
+            disabled={todos.value == "" && todos?.currentId == null}
+            onClick={() => updateTodo()}
+          >
+            Update
+          </button>
+        </div>
+        <ul>
+          {todos?.list?.map((todo, i) => (
+            <li key={i}>
+              <span>{todo}</span>
+              <div>
+                <button
+                  className="edt-btn"
+                  onClick={() =>
+                    setTodos({ ...todos, value: todo, currentId: i })
+                  }
+                >
+                  Edit
+                </button>
+                <button
+                  className="del-btn"
+                  onClick={() =>
+                    setTodos({
+                      ...todos,
+                      list: todos?.list.filter((todo, index) => i != index),
+                    })
+                  }
+                >
+                  Delete
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+        {todos?.list?.length != 0 && (
+          <div>
+            <span>Total Todos {todos?.list?.length}</span>
             <button
-              className="add-btn"
-              disabled={this.state.value.trim().length <= 0}
-              onClick={this.addTodo}
+              className="del-btn"
+              onClick={() => setTodos({ list: [], currentId: null, value: "" })}
             >
-              Add
-            </button>
-            <button
-              className="upd-btn"
-              disabled={this.state.currentId == null}
-              onClick={this.updateTodo}
-            >
-              Update
+              Delete ALL TODOS
             </button>
           </div>
-          <ul>
-            {this.state.todos.map((todo, i) => (
-              <li key={i}>
-                <span>{todo}</span>
-                <div>
-                  <button
-                    className="edt-btn"
-                    onClick={() => this.setState({ value: todo, currentId: i })}
-                  >
-                    Edit
-                  </button>
-                  <button
-                    className="del-btn"
-                    onClick={() => this.deleteTodo(i)}
-                  >
-                    Delete
-                  </button>
-                </div>
-              </li>
-            ))}
-          </ul>
-          {this.state.todos.length != 0 && (
-            <div>
-              <span>Total Todos {this?.state?.todos?.length}</span>
-              <button
-                className="del-btn"
-                onClick={() =>
-                  this.setState({ todos: [], currentId: null, value: "" })
-                }
-              >
-                Delete ALL TODOS
-              </button>
-            </div>
-          )}
-        </div>
+        )}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
