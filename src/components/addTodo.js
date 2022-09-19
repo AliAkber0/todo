02686f8bbf14b0bb1106ed13017bc9e9d2c4c298
Muhammad Kashif <muhammad.kashif72462@gmail.com>
2022-09-19 15:00:31 +0500

    user todo with axios

--- src/components/addTodo.js
+++ src/components/addTodo.js
@@ -1,41 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class AddTodo extends Component {
-    state = {
-        defaultValue: "",
-        value: this.props.addTodoValue
-    }
+  state = {
+    defaultValue: "",
+    post: {
+      name: "",
+      username: "",
+    },
+  };
 
-    handleChange = (e) => {
-        this.setState({
-            value: e.target.value
-        });
-    }
+  handleChange = ({ target }) => {
+    this.setState((prevState) => ({
+      post: {
+        ...prevState.post,
+        [target.name]: target.value,
+      },
+    }));
+  };
 
-    clearInput = () => {
+  addTodo = () => {
+    this.props.fooAddTodo(this.state.post);
+  };
+  render() {
+    const {
+      post: { content, title },
+    } = this.state;
 
-        document.getElementById("todoValue").value = "";
-        
-
-        this.setState({value:""});
-    }
-
-    addTodo = () => {
-        this.props.fooAddTodo(this.state.value);
-        this.clearInput();
-    }
-
-    render() {
-        return (
-            <div >
-                <input type="text"  id="todoValue" placeholder="ToDo" onChange={this.handleChange} />
-                <div >
-                    <button onClick={this.addTodo}  type="button" >Add New ToDo</button>
-                </div>
-                
-            </div>
-        );
-    }
+    return (
+      <div className="NewPost">
+        <h1>Add a User</h1>
+        <label>Name</label>
+        <input
+          type="text"
+          name="name"
+          value={title}
+          onChange={this.handleChange}
+        />
+        <label>username</label>
+        <input name="username" value={content} onChange={this.handleChange} />
+        <div className="add-button">
+          <button onClick={this.addTodo} className="add-button">
+            Add Post
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default AddTodo;
--- src/components/todos.js
+++ src/components/todos.js
@@ -1,83 +1,90 @@
 import React, { Component } from "react";
 import Todo from "./todo";
 import AddTodo from "./addTodo";
-import '../my-sass.scss'
-import userEvent from "@testing-library/user-event";
-import Users from './users'
+import "../my-sass.scss";
+import axios from "axios";
+
+import Users from "./users";
 class Todos extends Component {
   state = {
-    posts:[],
-    todos: [
-      { id: 1, value: "Todo Frist" },
-      { id: 2, value: "Todo second" },
-      { id: 3, value: "Todo Third" },
-      { id: 5, value: "Todo Fourth" },
-    ],
+    posts: [],
+    editing: false,
   };
-  getTime(){
-    let d=new Date();
-    let n=d.getTime();
+  getTime() {
+    let d = new Date();
+    let n = d.getTime();
     return n;
   }
-  handleDelete=todo=>{
-    const todos=this.state.todos.filter((t)=>{
-        return t.id!==todo
-    })
-    this.setState({todos})
-  }
-  addNewTodo = value => {
+  handleDelete = (todo) => {
+    let books = [];
+    axios
+      .delete(`https://jsonplaceholder.typicode.com/users/${todo}`)
+      .then((Response) => {
+        if (Response) {
+          books = this.state.books.filter((t) => {
+            return t.id !== todo;
+          });
+          this.setState({ books });
+        }
+      });
+  };
+  addNewTodo = (value) => {
     if (value) {
-        const todos = [...this.state.todos];
-        todos.push(
-            {
-                id: this.getTime(),
-                value: value,
-                isDone: false
-            }
-        );
-        this.setState({ addTodoValue: "", todos })
-    } else {
-        console.log("Please Add Todo Text");
+      axios
+        .post(`https://jsonplaceholder.typicode.com/users`, {
+          name: value.name,
+          username: value.username,
+        })
+        .then((Response) => {
+          const books = [...this.state.books];
+          books.push(Response.data);
+          this.setState({ books });
+        });
     }
-}
-  deleteAllTodo=()=>{
-    this.setState({todos:''})
-  }
-
+  };
+  deleteAllTodo = () => {
+    this.setState({ todos: "" });
+  };
+  updateHandler = (value) => {
+    const books = this.state.books.map((item) => {
+      if (item.id === value.id) {
+        return { ...item, name: value.content };
+      }
+      return item;
+    });
+    this.setState({ books });
+  };
 
   componentDidMount() {
-    fetch(`https://jsonplaceholder.typicode.com/posts`)
-    .then((response) => response.json())
-    .then(booksList => {
-        this.setState({ books: booksList });
+    axios(`https://jsonplaceholder.typicode.com/users`).then((Response) => {
+      this.setState({ books: Response.data });
     });
-}
+  }
 
   render() {
-
-    console.log(this.state.books,'books')
-    return(
-         <div> 
-
-                            {
-                    this.state.books?
-                    this.state.books.map((todo, index) => (
-                    
-                            <Users index={index+1} todo={todo} fooDelete={this.handleDelete} />
-                        
-                    )):null}
-                                        {/* <div className="tp-margin">
-                    <AddTodo fooAddTodo={this.addNewTodo} addTodoValue={this.state.addTodoValue}/>
-
-                    </div> */}
-                    {/* <div>
-                      <button onClick={this.deleteAllTodo} className="all-delete-btn">
-                        Delete All
-                      </button>
-                    </div> */}
-
-
-    </div>)
+    return (
+      <div className="outer-div">
+        <h2>Fetching Data with Axios</h2>
+        {this.state.books
+          ? this.state.books.map((todo, index) => (
+              <Users
+                index={index + 1}
+                todo={todo}
+                // key={todo.id}
+                fooDelete={this.handleDelete}
+                update={this.updateHandler}
+                editing={this.state.editing}
+              />
+            ))
+          : null}
+        <div>
+          <AddTodo
+            fooAddTodo={this.addNewTodo}
+            addTodoValue={this.state.valueid}
+          />
+        </div>
+      </div>
+    );
   }
 }
 export default Todos;
