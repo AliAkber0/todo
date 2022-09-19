f308398a2bdf6c944c207ae6eb6620d03fc512dd
AliAkber0 <aliakber654@gmail.com>
2022-09-19 17:58:58 +0500

    changes

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import "../../Styles/button.scss";
-const Button = ({ id, type, deleteTodoHandler, clickHandler, labelText }) => {
+const Button = ({
+  id,
+  type,
+  deleteTodoHandler,
+  clickHandler,
+  labelText,
+  editUser,
+}) => {
+  const getTypeMethod = () => {
+    switch (type) {
+      case "delete":
+        return () => deleteTodoHandler(id);
+      case "edit":
+        return () => editUser(id);
+      default:
+        return clickHandler;
+    }
+  };
   return (
     <button
       className={type === "delete" ? "button-delete" : "button"}
-      onClick={type === "delete" ? () => deleteTodoHandler(id) : clickHandler}
+      onClick={getTypeMethod}
     >
       {labelText}
     </button>
