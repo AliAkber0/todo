2668de51b3f0eed3564240592b11bc3019d2924a
AliAkber0 <aliakber654@gmail.com>
2022-09-20 17:19:13 +0500

    redux saga

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React from "react";
 import User from "./Screens/User/User";
 import "./Styles/app.scss";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="app">
-        <h3>User App Redux Thunk</h3>
-        <User />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="app">
+      <h3>User App Redux Thunk</h3>
+      <User />
+    </div>
+  );
+};
 
 export default App;
--- src/Screens/User/User.jsx
+++ src/Screens/User/User.jsx
@@ -1,40 +1,38 @@
 import React from "react";
 import AddUser from "../../Components/AddUser/AddUser";
 import ShowUsers from "../../Components/ShowUsers/ShowUsers";
-import "../../Styles/user.scss";
 import { setUser } from "../../ReduxStore/Action/Actions";
-import { connect } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import "../../Styles/user.scss";
 
-class User extends React.Component {
-  render() {
-    return (
-      <div className="todo">
-        <AddUser />
-        {this.props?.isLoading && (
-          <div className="loading-text">{this.props.loadingMessage}</div>
-        )}
-        {this.props?.error && (
-          <div className="error-text">{this.props.error}</div>
-        )}
-        <ShowUsers />
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  return {
-    isLoading: state?.isLoading,
-    loadingMessage: state?.loadingMessage,
-    userList: state?.userList,
-    error: state?.error,
-  };
+const User = () => {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state);
+  return (
+    <div className="todo">
+      <AddUser />
+      {user.isLoading && (
+        <div className="loading-text">{user.loadingMessage}</div>
+      )}
+      {this.props?.error && <div className="error-text">{user.error}</div>}
+      <ShowUsers />
+    </div>
+  );
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    addUser: (user) => dispatch(setUser(user)),
-  };
-};
+// const mapStateToProps = (state) => {
+//   return {
+//     isLoading: state?.isLoading,
+//     loadingMessage: state?.loadingMessage,
+//     userList: state?.userList,
+//     error: state?.error,
+//   };
+// };
 
-export default connect(mapStateToProps, mapDispatchToProps)(User);
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     addUser: (user) => dispatch(setUser(user)),
+//   };
+// };
+
+export default User;
